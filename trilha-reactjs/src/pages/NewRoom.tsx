153e310b3d005083a8fd2fcc4919e0fbace2604b
KRochaS <karinerocha.s@hotmail.com>
2021-06-22 23:27:51 -0300

    NewRoom

--- trilha-reactjs/src/pages/NewRoom.tsx
+++ trilha-reactjs/src/pages/NewRoom.tsx
@@ -1,41 +1,39 @@
 import '../styles/auth.scss';
 
-import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 
 import illustration from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import { Button } from '../components/Button';
-import { AuthContext } from '../contexts/AuthContext';
+import { useAuth } from '../hooks/useAuth';
 
 export function NewRoom() {
-    // const {user} = useContext(AuthContext);
-   
-    
+    const { user } = useAuth();
+
     return (
         <div id="page-auth">
-            <aside> 
+            <aside>
                 <img src={illustration} alt="Ilustração simbolizando perguntas e respostas" />
                 <strong> Crie salas de Q&amp;A ao vivo</strong>
                 <p> Tire as dúvidas da sua audiência em tempo real</p>
             </aside>
             <main>
-                <div className="main-content"> 
+                <div className="main-content">
                     <img src={logoImg} alt="Letmeask" />
-                   <h2> Criar uma nova sala</h2>
+                    <h2> Criar uma nova sala</h2>
                     <form>
-                        <input 
+                        <input
                             type="text"
                             placeholder="Nome da sala"
-                        
+
                         />
-                       <Button type="submit"> 
-                           Criar sala
-                       </Button>
+                        <Button type="submit">
+                            Criar sala
+                        </Button>
                     </form>
                     <p>
                         Quer entrar em uma sala existente?
-                            
+
                         <Link to="/"> clique aqui </Link>
                     </p>
                 </div>
