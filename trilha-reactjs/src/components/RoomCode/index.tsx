c2961d2153bcf136592cbc9f51a192b52c2cfa48
KRochaS <karinerocha.s@hotmail.com>
2021-07-13 22:36:59 -0300

    RoomCode darkMode

--- trilha-reactjs/src/components/RoomCode/index.tsx
+++ trilha-reactjs/src/components/RoomCode/index.tsx
@@ -1,6 +1,7 @@
 import './styles.scss';
 
 import copyImg from '../../assets/images/copy.svg';
+import { useTheme } from '../../hooks/useTheme';
 
 
 type RoomCodeProps = {
@@ -8,13 +9,14 @@ type RoomCodeProps = {
 }
 
 export function RoomCode(props: RoomCodeProps) {
+    const { theme } = useTheme();
 
     function copyRoomCodeToClipboard() {
         navigator.clipboard.writeText(props.code);
     }
 
     return (
-        <button title="Copiar código" className="room-code" onClick={copyRoomCodeToClipboard}>
+        <button title="Copiar código" className={"room-code " + theme} onClick={copyRoomCodeToClipboard}>
             <div>
                 <img src={copyImg} alt="Copiar código da sala" />
             </div>
