17498ce8822277226402143c0402a365fa725248
KRochaS <karinerocha.s@hotmail.com>
2021-07-18 16:56:23 -0300

    toast

--- trilha-reactjs/src/components/RoomCode/index.tsx
+++ trilha-reactjs/src/components/RoomCode/index.tsx
@@ -2,6 +2,7 @@ import './styles.scss';
 
 import copyImg from '../../assets/images/copy.svg';
 import { useTheme } from '../../hooks/useTheme';
+import toast, { Toaster } from 'react-hot-toast';
 
 
 type RoomCodeProps = {
@@ -13,15 +14,21 @@ export function RoomCode(props: RoomCodeProps) {
 
     function copyRoomCodeToClipboard() {
         navigator.clipboard.writeText(props.code);
+        toast.success('Código copiado');
     }
 
     return (
-        <button title="Copiar código" className={"room-code " + theme} onClick={copyRoomCodeToClipboard}>
-            <div>
-                <img src={copyImg} alt="Copiar código da sala" />
-            </div>
-            <span className="d-none d-lg-block d-md-block">Sala #{props.code}</span>
-        </button>
+
+        <>
+            <div><Toaster /></div>
+            <button title="Copiar código" className={"room-code " + theme} onClick={copyRoomCodeToClipboard}>
+                <div>
+                    <img src={copyImg} alt="Copiar código da sala" />
+                </div>
+                <span className="d-none d-lg-block d-md-block">Sala #{props.code}</span>
+            </button>
+        </>
+
 
     )
 }
